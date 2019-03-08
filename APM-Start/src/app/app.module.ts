78f3fd7ba0fe4c1950bdecc3248110234a8657cb
Jonathan Edwards <Jonathan.Edwards@navihealth.com>
2019-03-08 14:30:35 -0600

    inital commit

---
+++ APM-Start/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductListComponent } from './products/product-list.component';
+
+@NgModule({
+    declarations: [AppComponent, WelcomeComponent, ProductListComponent],
+    imports: [BrowserModule, FormsModule],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
